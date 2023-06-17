e4070aac718891452914265a3ee07c1e13b63c02
Nadim Hairi <dewdadim@gmail.com>
2023-06-18 03:35:05 +0800

    connect html with php

--- register-owner.php
+++ register-owner.php
@@ -9,16 +9,16 @@
         $password_confirmation = $_POST["confirmpass"];
 
         if($password === $password_confirmation) {
-            $sql = "INSERT INTO Customer VALUES('$name', '$email', '$phone', '$password')";
+            $sql = "INSERT INTO RoomOwner VALUES('$name', '$email', '$phone', '$password')";
             $result = mysqli_query($connect, $sql);
 
             if ($result) {
-                echo "<script>alert('Sign Up successful!')</script>";
+                echo "<script>alert('Register successful!')</script>";
                 echo "<script>window.location='login.html'</script>";
             }   
 
             else {
-                echo "<script>alert('Sign Up not successful')</script>"; 
+                echo "<script>alert('Register not successful')</script>"; 
                 echo "<script>window.location='login.php'</script>";
             }
                 
