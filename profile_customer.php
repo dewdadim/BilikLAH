20f40be7ec37de31ee64b077064b0bdc19e50849
hanif0000 <hanif041220@gmail.com>
2023-06-18 11:44:49 +0800

    .

--- profile_customer.php
+++ profile_customer.php
@@ -1,31 +1,34 @@
 <?php
 include('php/connect.php');
 
+
 $customerName = $_POST['customerName'];
 $customerPhone = $_POST['customerPhone'];
 $customerPassword = $_POST['customerPassword'];
 $customerEmail = $_POST['customerEmail'];
 
 
-$updateQuery = "UPDATE Customer SET customerName = '$customerName' WHERE customerId = 999";
+$updateQuery = "UPDATE Customer SET customerName = '$customerName', customerPhone = '$customerPhone', customerPassword = '$customerPassword', customerEmail = '$customerEmail' WHERE customerId = $customerId";
  
  
-if (mysqli_query($link,$updateQuery)) {
-    echo "Name updated successfully.";
+if (mysqli_query($connect,$updateQuery)) {
+    echo "Fields updated successfully.";
 } else {
-    echo "Error updating name: " . mysqli_error($link);
+    echo "Error updating fields: " . mysqli_error($link);
 }
 
 // Close the database connection
-mysqli_close($link);
+mysqli_close($connect);
+
+
+$customerName="SELECT customerName FROM Customer WHERE Phone = '123456789'";
+
+
+
+
+
+
+
+
+
 ?>
-
-
-
-
-
-
-
-
-
- 
